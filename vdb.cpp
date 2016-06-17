c1a47c20e6258319eb9273898adb89386c09b444
Simen H <simenh.jobb@gmail.com>
2016-06-17 14:53:00 +0200

    Can disable imgui cursor now

--- vdb.cpp
+++ vdb.cpp
@@ -295,6 +295,8 @@ void vdb(char *Label, vdb_callback Callback)
     bool MainMenuActive = false;
     bool Running = true;
 
+    bool ImGuiCursor = ImGui::GetIO().MouseDrawCursor;
+
     bool        SaveScreenshot = false;
     static bool ScreenshotDrawCursor = false;
     static bool ScreenshotDrawGui = false;
@@ -339,7 +341,8 @@ void vdb(char *Label, vdb_callback Callback)
         else
         {
             ImGui::GetStyle().Alpha = 1.0f;
-            ImGui::GetIO().MouseDrawCursor = true;
+            if (ImGuiCursor)
+                ImGui::GetIO().MouseDrawCursor = true;
         }
         ImGui_ImplSdl_NewFrame(Window.SDLWindow);
 
