93ded31a46a2bb150ae892d02238f54c57a84431
lightbits <simenh.jobb@gmail.com>
2018-06-26 13:47:23 +0200

    vdb and im config

---
+++ build.bat
@@ -0,0 +1,26 @@
+:: Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
+:: Place a copy of SDL2 for your platform (x86 or x64) in the directory of your final linked executable
+:: Prebuilt binaries can be found in src/sdl/
+@echo off
+
+:: Create a .build directory (hidden)
+if not exist ".build" mkdir .build
+attrib +h .build /s /d
+pushd .build
+
+:: -Od: Turns off optimizations and speeds up compilation
+:: -Zi: Generates debug symbols
+:: -WX: Treat warnings as errors
+:: -MD: Use DLL run-time library
+set INC=-I..\src\sdl\ -I..\include\ -I..\include\vdb\
+set CF=%INC% -Zi -nologo -Od -WX -W3 -wd4100 -wd4189 -wd4996 -wd4055
+
+:: -subsystem:console: Open a console
+:: -debug: Create debugging information into .pdb
+set SDLLIB=..\src\sdl\vc2010-x86\SDL2.lib ..\src\sdl\vc2010-x86\SDL2main.lib
+set LF=%SDLLIB% opengl32.lib user32.lib gdi32.lib shell32.lib
+
+cl ..\src\vdb.cpp -c %CF%
+lib vdb.obj %LF% -nologo -out:vdb.lib
+
+popd
