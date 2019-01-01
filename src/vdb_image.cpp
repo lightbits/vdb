a4a41de989c35572dcb82631dd8c720eea802296
lightbits <simenh.jobb@gmail.com>
2019-01-01 15:54:50 +0100

    vdb_image is core compatible

--- src/vdb_image.cpp
+++ src/vdb_image.cpp
@@ -1,7 +1,7 @@
 void vdbLoadImageUint8(int slot, const void *data, int width, int height, int channels)
 {
     assert(channels >= 1 && channels <= 4 && "'channels' must be 1,2,3 or 4");
-    if (channels == 1)      SetTexture(slot, data, width, height, GL_LUMINANCE, GL_UNSIGNED_BYTE, GL_NEAREST, GL_NEAREST);
+    if (channels == 1)      SetTexture(slot, data, width, height, GL_RED, GL_UNSIGNED_BYTE, GL_NEAREST, GL_NEAREST);
     else if (channels == 2) SetTexture(slot, data, width, height, GL_RG, GL_UNSIGNED_BYTE, GL_NEAREST, GL_NEAREST);
     else if (channels == 3) SetTexture(slot, data, width, height, GL_RGB, GL_UNSIGNED_BYTE, GL_NEAREST, GL_NEAREST);
     else if (channels == 4) SetTexture(slot, data, width, height, GL_RGBA, GL_UNSIGNED_BYTE, GL_NEAREST, GL_NEAREST);
@@ -9,7 +9,7 @@ void vdbLoadImageUint8(int slot, const void *data, int width, int height, int ch
 void vdbLoadImageFloat32(int slot, const void *data, int width, int height, int channels)
 {
     assert(channels >= 1 && channels <= 4 && "'channels' must be 1,2,3 or 4");
-    if (channels == 1)      SetTexture(slot, data, width, height, GL_LUMINANCE, GL_FLOAT, GL_NEAREST, GL_NEAREST);
+    if (channels == 1)      SetTexture(slot, data, width, height, GL_RED, GL_FLOAT, GL_NEAREST, GL_NEAREST);
     else if (channels == 2) SetTexture(slot, data, width, height, GL_RG, GL_FLOAT, GL_NEAREST, GL_NEAREST);
     else if (channels == 3) SetTexture(slot, data, width, height, GL_RGB, GL_FLOAT, GL_NEAREST, GL_NEAREST);
     else if (channels == 4) SetTexture(slot, data, width, height, GL_RGBA, GL_FLOAT, GL_NEAREST, GL_NEAREST);
@@ -28,35 +28,32 @@ void vdbLoadImageFromFile(int slot, const char *filename, int *width, int *heigh
 }
 void vdbDrawImage(int slot)
 {
-    glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, GetTextureSlotHandle(slot));
-    glBegin(GL_TRIANGLES);
+    vdbTriangles();
     #if VDB_FLIP_IMAGE_TEXEL_Y==1
-    glColor4f(1,1,1,1); glTexCoord2f(0,1);glVertex2f(-1,-1);
-    glColor4f(1,1,1,1); glTexCoord2f(1,1);glVertex2f(+1,-1);
-    glColor4f(1,1,1,1); glTexCoord2f(1,0);glVertex2f(+1,+1);
-    glColor4f(1,1,1,1); glTexCoord2f(1,0);glVertex2f(+1,+1);
-    glColor4f(1,1,1,1); glTexCoord2f(0,0);glVertex2f(-1,+1);
-    glColor4f(1,1,1,1); glTexCoord2f(0,1);glVertex2f(-1,-1);
+    vdbColor(1,1,1,1); vdbTexel(0,1); vdbVertex(-1,-1);
+    vdbColor(1,1,1,1); vdbTexel(1,1); vdbVertex(+1,-1);
+    vdbColor(1,1,1,1); vdbTexel(1,0); vdbVertex(+1,+1);
+    vdbColor(1,1,1,1); vdbTexel(1,0); vdbVertex(+1,+1);
+    vdbColor(1,1,1,1); vdbTexel(0,0); vdbVertex(-1,+1);
+    vdbColor(1,1,1,1); vdbTexel(0,1); vdbVertex(-1,-1);
     #else
-    glColor4f(1,1,1,1); glTexCoord2f(0,0);glVertex2f(-1,-1);
-    glColor4f(1,1,1,1); glTexCoord2f(1,0);glVertex2f(+1,-1);
-    glColor4f(1,1,1,1); glTexCoord2f(1,1);glVertex2f(+1,+1);
-    glColor4f(1,1,1,1); glTexCoord2f(1,1);glVertex2f(+1,+1);
-    glColor4f(1,1,1,1); glTexCoord2f(0,1);glVertex2f(-1,+1);
-    glColor4f(1,1,1,1); glTexCoord2f(0,0);glVertex2f(-1,-1);
+    vdbColor(1,1,1,1); vdbTexel(0,0); vdbVertex(-1,-1);
+    vdbColor(1,1,1,1); vdbTexel(1,0); vdbVertex(+1,-1);
+    vdbColor(1,1,1,1); vdbTexel(1,1); vdbVertex(+1,+1);
+    vdbColor(1,1,1,1); vdbTexel(1,1); vdbVertex(+1,+1);
+    vdbColor(1,1,1,1); vdbTexel(0,1); vdbVertex(-1,+1);
+    vdbColor(1,1,1,1); vdbTexel(0,0); vdbVertex(-1,-1);
     #endif
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
+    vdbEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 void vdbBindImage(int slot)
 {
-    glEnable(GL_TEXTURE_2D);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, GetTextureSlotHandle(slot));
 }
 void vdbUnbindImage()
 {
-    glDisable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, 0);
 }
