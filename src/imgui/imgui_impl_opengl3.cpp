719550cdef4b501a625c31c26da49b9eeb96279e
lightbits <simenh.jobb@gmail.com>
2019-03-26 11:28:31 +0100

    windows-bugfix:imgui update, print shader log

--- src/imgui/imgui_impl_opengl3.cpp
+++ src/imgui/imgui_impl_opengl3.cpp
@@ -365,13 +365,15 @@ static bool CheckShader(GLuint handle, const char* desc)
     glGetShaderiv(handle, GL_COMPILE_STATUS, &status);
     glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &log_length);
     if ((GLboolean)status == GL_FALSE)
-        fprintf(stderr, "ERROR: ImGui_ImplOpenGL3_CreateDeviceObjects: failed to compile %s!\n", desc);
-    if (log_length > 0)
     {
-        ImVector<char> buf;
-        buf.resize((int)(log_length + 1));
-        glGetShaderInfoLog(handle, log_length, NULL, (GLchar*)buf.begin());
-        fprintf(stderr, "%s\n", buf.begin());
+        fprintf(stderr, "ERROR: ImGui_ImplOpenGL3_CreateDeviceObjects: failed to compile %s!\n", desc);
+        if (log_length > 0)
+        {
+            ImVector<char> buf;
+            buf.resize((int)(log_length + 1));
+            glGetShaderInfoLog(handle, log_length, NULL, (GLchar*)buf.begin());
+            fprintf(stderr, "%s\n", buf.begin());
+        }
     }
     return (GLboolean)status == GL_TRUE;
 }
@@ -383,13 +385,15 @@ static bool CheckProgram(GLuint handle, const char* desc)
     glGetProgramiv(handle, GL_LINK_STATUS, &status);
     glGetProgramiv(handle, GL_INFO_LOG_LENGTH, &log_length);
     if ((GLboolean)status == GL_FALSE)
-        fprintf(stderr, "ERROR: ImGui_ImplOpenGL3_CreateDeviceObjects: failed to link %s! (with GLSL '%s')\n", desc, g_GlslVersionString);
-    if (log_length > 0)
     {
-        ImVector<char> buf;
-        buf.resize((int)(log_length + 1));
-        glGetProgramInfoLog(handle, log_length, NULL, (GLchar*)buf.begin());
-        fprintf(stderr, "%s\n", buf.begin());
+        fprintf(stderr, "ERROR: ImGui_ImplOpenGL3_CreateDeviceObjects: failed to link %s! (with GLSL '%s')\n", desc, g_GlslVersionString);
+        if (log_length > 0)
+        {
+            ImVector<char> buf;
+            buf.resize((int)(log_length + 1));
+            glGetProgramInfoLog(handle, log_length, NULL, (GLchar*)buf.begin());
+            fprintf(stderr, "%s\n", buf.begin());
+        }
     }
     return (GLboolean)status == GL_TRUE;
 }
