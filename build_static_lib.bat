d32d5063e1937f9f8a327a9a87d17026b52b6ebe
lightbits <simenh.jobb@gmail.com>
2019-05-04 11:31:15 +0200

    move freetype

--- build_static_lib.bat
+++ build_static_lib.bat
@@ -3,7 +3,7 @@
 if not exist "lib" mkdir lib
 pushd lib
 
-set INCLUDES=-I..\include\ -I..\include\vdb\ -I%SDL2_DIR%\include -I..\lib\freetype\include
+set INCLUDES=-I..\include\ -I..\include\vdb\ -I%SDL2_DIR%\include -I..\src\freetype\include
 set FLAGS=-MD -Zi -Oi -fp:fast -nologo -O2 -WX -W3 -wd4100 -wd4189 -wd4996 -wd4055
 
 cl %INCLUDES% %FLAGS% -c ..\src\vdb.cpp
