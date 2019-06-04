f431b7794636d01ee33b61535178e4cacf9112d8
lightbits <simenh.jobb@gmail.com>
2019-06-04 21:18:07 +0200

    header file spacing

--- include/vdb.h
+++ include/vdb.h
@@ -24,175 +24,174 @@ struct vdbTextureOptions
     vdbTextureOptions() : vmin(), vmax(), gather(), cmap(0), filter(VDB_LINEAR), wrap(VDB_CLAMP) { }
 };
 
-void vdbDetachGLContext();
-void vdbStepOnce();
-void vdbStepOver();
-bool vdbBeginFrame(const char *label);
-void vdbEndFrame();
-bool vdbIsFirstFrame();
-bool vdbIsDifferentLabel();
+void    vdbDetachGLContext();
+void    vdbStepOnce();
+void    vdbStepOver();
+bool    vdbBeginFrame(const char *label);
+void    vdbEndFrame();
+bool    vdbIsFirstFrame();
+bool    vdbIsDifferentLabel();
 vdbVec2 vdbGetRenderScale(); // See FAQ:RenderScale below
 vdbVec2 vdbGetRenderOffset(); // See FAQ:RenderOffset below
 
 // immediate mode 2D/3D drawing API
-void vdbInverseColor(bool enable);
-void vdbClearColor(float r, float g, float b, float a=1.0f);
-void vdbClearDepth(float d);
-void vdbBlendNone();
-void vdbBlendAdd();
-void vdbBlendAlpha();
-void vdbCullFace(bool enable);       // if enabled, back-facing triangles are not drawn
-void vdbDepthTest(bool enable);      // if enabled, fragments are tested against depth buffer
-void vdbDepthWrite(bool enable);     // if enabled, fragments write to the depth buffer
-void vdbLineWidth(float width);      // line diameter in framebuffer pixels
-void vdbPointSegments(int segments); // points can be rendered as triangles (segments=3), quads (segments=4) or circles or varying fineness (segments > 4)
-void vdbPointSize(float size);       // point diameter in framebuffer pixels (size=1 and segments=4 gives pixel-perfect rendering)
-void vdbPointSize3D(float size);     // point diameter in model-coordinates (is affected by projection)
-void vdbBeginLines();
-void vdbBeginPoints();
-void vdbBeginTriangles();
-void vdbEnd();
-void vdbBeginList(int list); // call before vdbBegin* to store the resulting geometry stream to a buffer (you can draw the buffer by calling vdbDrawList)
-void vdbDrawList(int list);
-void vdbVertex(float x, float y, float z=0.0f, float w=1.0f);
-void vdbVertex2fv(float *v, float z=0.0f, float w=1.0f);
-void vdbVertex3fv(float *v, float w=1.0f);
-void vdbVertex4fv(float *v);
-void vdbVertex(vdbVec3 xyz, float w=1.0f);
-void vdbVertex(vdbVec4 xyzw);
-void vdbColor4ub(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
-void vdbColor4ubv(unsigned char *v);
-void vdbColor3ubv(unsigned char *v, unsigned char a=255);
-void vdbColor(float r, float g, float b, float a=1.0f);
-void vdbColor4fv(float *v);
-void vdbColor3fv(float *v, float a=1.0f);
-void vdbColor(vdbVec3 rgb, float a=1.0f);
-void vdbColor(vdbVec4 rgba);
-void vdbTexel(float u, float v);
+void    vdbInverseColor(bool enable);
+void    vdbClearColor(float r, float g, float b, float a=1.0f);
+void    vdbClearDepth(float d);
+void    vdbBlendNone();
+void    vdbBlendAdd();
+void    vdbBlendAlpha();
+void    vdbCullFace(bool enable);       // if enabled, back-facing triangles are not drawn
+void    vdbDepthTest(bool enable);      // if enabled, fragments are tested against depth buffer
+void    vdbDepthWrite(bool enable);     // if enabled, fragments write to the depth buffer
+void    vdbLineWidth(float width);      // line diameter in framebuffer pixels
+void    vdbPointSegments(int segments); // points can be rendered as triangles (segments=3), quads (segments=4) or circles or varying fineness (segments > 4)
+void    vdbPointSize(float size);       // point diameter in framebuffer pixels (size=1 and segments=4 gives pixel-perfect rendering)
+void    vdbPointSize3D(float size);     // point diameter in model-coordinates (is affected by projection)
+void    vdbBeginLines();
+void    vdbBeginPoints();
+void    vdbBeginTriangles();
+void    vdbEnd();
+void    vdbBeginList(int list); // call before vdbBegin* to store the resulting geometry stream to a buffer (you can draw the buffer by calling vdbDrawList)
+void    vdbDrawList(int list);
+void    vdbVertex(float x, float y, float z=0.0f, float w=1.0f);
+void    vdbVertex2fv(float *v, float z=0.0f, float w=1.0f);
+void    vdbVertex3fv(float *v, float w=1.0f);
+void    vdbVertex4fv(float *v);
+void    vdbVertex(vdbVec3 xyz, float w=1.0f);
+void    vdbVertex(vdbVec4 xyzw);
+void    vdbColor4ub(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+void    vdbColor4ubv(unsigned char *v);
+void    vdbColor3ubv(unsigned char *v, unsigned char a=255);
+void    vdbColor(float r, float g, float b, float a=1.0f);
+void    vdbColor4fv(float *v);
+void    vdbColor3fv(float *v, float a=1.0f);
+void    vdbColor(vdbVec3 rgb, float a=1.0f);
+void    vdbColor(vdbVec4 rgba);
+void    vdbTexel(float u, float v);
 
 // immediate mode utilities
-void vdbNoteV(float x, float y, const char *fmt, va_list args);
-void vdbNote(float x, float y, const char *fmt, ...);
-void vdbFillArc(vdbVec3 base, vdbVec3 p1, vdbVec3 p2, int segments=8);
-void vdbLineCube(float size_x, float size_y, float size_z); // Draws a cube from [-size/2, +size/2] in each axis. Do not call vdbBegin|EndLines before|after.
-void vdbLineCube(vdbVec3 p_min, vdbVec3 p_max);
-void vdbLineGrid(float x_min, float x_max, float y_min, float y_max, int n);
-void vdbLineRect(float x, float y, float size_x, float size_y);
-void vdbFillRect(float x, float y, float size_x, float size_y);
-void vdbLineCircle(float x, float y, float radius, int segments=16);
+void    vdbNoteV(float x, float y, const char *fmt, va_list args);
+void    vdbNote(float x, float y, const char *fmt, ...);
+void    vdbFillArc(vdbVec3 base, vdbVec3 p1, vdbVec3 p2, int segments=8);
+void    vdbLineCube(float size_x, float size_y, float size_z); // Draws a cube from [-size/2, +size/2] in each axis. Do not call vdbBegin|EndLines before|after.
+void    vdbLineCube(vdbVec3 p_min, vdbVec3 p_max);
+void    vdbLineGrid(float x_min, float x_max, float y_min, float y_max, int n);
+void    vdbLineRect(float x, float y, float size_x, float size_y);
+void    vdbFillRect(float x, float y, float size_x, float size_y);
+void    vdbLineCircle(float x, float y, float radius, int segments=16);
 
 // matrix stack
-// VDB can be compiled to accept matrices in either row- or column-major memory order. See vdb_config.h
+// vdb can be compiled to accept matrices in either row- or column-major memory order. See vdb_config.h
 // Matrix = transformation from model coordinates to view coordinates (before projection)
-void vdbPushMatrix();                         // Push matrix stack by one (current top is copied)
-void vdbPopMatrix();                          // Pop matrix stack by one (previous top is restored)
-void vdbProjection(float *m);                 // NULL -> Load 4x4 identity matrix
-void vdbLoadMatrix(float *m);                 // NULL -> Load 4x4 identity matrix
-void vdbMultMatrix(float *m);                 // Matrix <- Matrix mul m (right-multiply top of matrix stack)
-void vdbGetMatrix(float *m);                  // You allocate m, e.g.: float matrix[4*4]; vdbGetMatrix(matrix);
-void vdbGetProjection(float *m);              // You allocate m, e.g.: float projection[4*4]; vdbGetProjection(projection);
-void vdbGetPVM(float *m);                     // You allocate m, e.g.: float pvm[4*4]; vdbGetPVM(pvm);
-void vdbTranslate(float x, float y, float z); // Matrix <- Matrix mul Translate(x,y,z)
-void vdbRotateXYZ(float x, float y, float z); // Matrix <- Matrix mul Rx(x) mul Ry(y) mul Rz(z)
-void vdbRotateZYX(float z, float y, float x); // Matrix <- Matrix mul Rz(z) mul Ry(y) mul Rx(x)
+void    vdbPushMatrix();                         // Push matrix stack by one (current top is copied)
+void    vdbPopMatrix();                          // Pop matrix stack by one (previous top is restored)
+void    vdbProjection(float *m);                 // NULL -> Load 4x4 identity matrix
+void    vdbLoadMatrix(float *m);                 // NULL -> Load 4x4 identity matrix
+void    vdbMultMatrix(float *m);                 // Matrix <- Matrix mul m (right-multiply top of matrix stack)
+void    vdbGetMatrix(float *m);                  // You allocate m, e.g.: float matrix[4*4]; vdbGetMatrix(matrix);
+void    vdbGetProjection(float *m);              // You allocate m, e.g.: float projection[4*4]; vdbGetProjection(projection);
+void    vdbGetPVM(float *m);                     // You allocate m, e.g.: float pvm[4*4]; vdbGetPVM(pvm);
+void    vdbTranslate(float x, float y, float z); // Matrix <- Matrix mul Translate(x,y,z)
+void    vdbRotateXYZ(float x, float y, float z); // Matrix <- Matrix mul Rx(x) mul Ry(y) mul Rz(z)
+void    vdbRotateZYX(float z, float y, float x); // Matrix <- Matrix mul Rz(z) mul Ry(y) mul Rx(x)
 
 // matrix stack utilities
-void vdbOrtho(float x_left, float x_right, float y_bottom, float y_top, float z_near=-1.0f, float z_far=+1.0f);
-void vdbPerspective(float yfov, float z_near, float z_far, float x_offset=0.0f, float y_offset=0.0f); // x_offset and y_offset shifts all geometry by a given amount in NDC units (shift is independent of depth)
-// These *overwrite* the view model matrix, they don't multiply
-void vdbCamera2D(float init_radius=1.0f);
-void vdbCameraTrackball(float init_radius=1.0f);
-void vdbCameraTurntable(float init_radius=1.0f, vdbVec3 look_at=vdbVec3());
+void    vdbOrtho(float x_left, float x_right, float y_bottom, float y_top, float z_near=-1.0f, float z_far=+1.0f);
+void    vdbPerspective(float yfov, float z_near, float z_far, float x_offset=0.0f, float y_offset=0.0f); // x_offset and y_offset shifts all geometry by a given amount in NDC units (shift is independent of depth)
+void    vdbCamera2D(float init_radius=1.0f); // Obs! overwrites the current view model matrix
+void    vdbCameraTrackball(float init_radius=1.0f); // Obs! overwrites the current view model matrix
+void    vdbCameraTurntable(float init_radius=1.0f, vdbVec3 look_at=vdbVec3()); // Obs! overwrites the current view model matrix
 
 // viewport and viewport conversions
-void vdbViewporti(int left, int bottom, int width, int height);          // Map all subsequent rendering operations to this region of the window (framebuffer units, not window units)
-void vdbViewport(float left, float bottom, float width, float height);   // Window-size indpendent version of the above (coordinates are in the range [0,1])
+void    vdbViewporti(int left, int bottom, int width, int height);          // Map all subsequent rendering operations to this region of the window (framebuffer units, not window units)
+void    vdbViewport(float left, float bottom, float width, float height);   // Window-size indpendent version of the above (coordinates are in the range [0,1])
 vdbVec2 vdbNDCToWindow(float xn, float yn);
 vdbVec2 vdbWindowToNDC(float xw, float yw);
 vdbVec3 vdbNDCToModel(float x_ndc, float y_ndc, float depth=-1.0f);
 vdbVec2 vdbModelToNDC(float x, float y, float z=0.0f, float w=1.0f);
-float vdbGetAspectRatio();
-int vdbGetFramebufferWidth();
-int vdbGetFramebufferHeight();
-int vdbGetWindowWidth(); // Note: the window size may not be the same as the framebuffer resolution on retina displays.
-int vdbGetWindowHeight();
+float   vdbGetAspectRatio();
+int     vdbGetFramebufferWidth();
+int     vdbGetFramebufferHeight();
+int     vdbGetWindowWidth(); // Note: the window size may not be the same as the framebuffer resolution on retina displays.
+int     vdbGetWindowHeight();
 
 // keyboard
-bool vdbWasKeyPressed(vdbKey key);
-bool vdbWasKeyReleased(vdbKey key);
-bool vdbIsKeyDown(vdbKey key);
+bool    vdbWasKeyPressed(vdbKey key);
+bool    vdbWasKeyReleased(vdbKey key);
+bool    vdbIsKeyDown(vdbKey key);
 
 // mouse
-bool vdbWasMouseOver(float x, float y, float z=0.0f, float w=1.0f);
-int vdbGetMouseOverIndex(float *x=0, float *y=0, float *z=0);
+bool    vdbWasMouseOver(float x, float y, float z=0.0f, float w=1.0f);
+int     vdbGetMouseOverIndex(float *x=0, float *y=0, float *z=0);
 vdbVec2 vdbGetMousePos();    // upper-left: (0,0). bottom-right: (WindowWidth, WindowHeight)
 vdbVec2 vdbGetMousePosNDC(); // bottom-left: (-1,-1). top-right: (+1,+1)
 vdbVec3 vdbGetMousePosModel(float depth=-1.0f);
-float vdbGetMouseWheel();
-bool vdbWasMouseLeftPressed();
-bool vdbWasMouseRightPressed();
-bool vdbWasMouseMiddlePressed();
-bool vdbWasMouseLeftReleased();
-bool vdbWasMouseRightReleased();
-bool vdbWasMouseMiddleReleased();
-bool vdbIsMouseLeftDown();
-bool vdbIsMouseRightDown();
-bool vdbIsMouseMiddleDown();
+float   vdbGetMouseWheel();
+bool    vdbWasMouseLeftPressed();
+bool    vdbWasMouseRightPressed();
+bool    vdbWasMouseMiddlePressed();
+bool    vdbWasMouseLeftReleased();
+bool    vdbWasMouseRightReleased();
+bool    vdbWasMouseMiddleReleased();
+bool    vdbIsMouseLeftDown();
+bool    vdbIsMouseRightDown();
+bool    vdbIsMouseMiddleDown();
 
 // image
-void vdbLoadImageUint8(int slot, const void *data, int width, int height, int channels);
-void vdbLoadImageFloat32(int slot, const void *data, int width, int height, int channels);
-void vdbLoadImageFromFile(int slot, const char *filename, int *width=0, int *height=0, int *channels=0);
-void vdbUnbindImage();
-void vdbBindImage(int slot, vdbTextureFilter filter=VDB_LINEAR, vdbTextureWrap wrap=VDB_CLAMP);
-void vdbDrawImage(int slot, vdbTextureFilter filter=VDB_LINEAR, vdbTextureWrap wrap=VDB_CLAMP);
-void vdbDrawImage(int slot, vdbTextureOptions options);
+void    vdbLoadImageUint8(int slot, const void *data, int width, int height, int channels);
+void    vdbLoadImageFloat32(int slot, const void *data, int width, int height, int channels);
+void    vdbLoadImageFromFile(int slot, const char *filename, int *width=0, int *height=0, int *channels=0);
+void    vdbUnbindImage();
+void    vdbBindImage(int slot, vdbTextureFilter filter=VDB_LINEAR, vdbTextureWrap wrap=VDB_CLAMP);
+void    vdbDrawImage(int slot, vdbTextureFilter filter=VDB_LINEAR, vdbTextureWrap wrap=VDB_CLAMP);
+void    vdbDrawImage(int slot, vdbTextureOptions options);
 
 // shader
-void vdbLoadShader(int slot, const char *fragment_shader_source_string);
-void vdbBeginShader(int slot);
-void vdbEndShader();
-void vdbUniform1f(const char *name, float x);
-void vdbUniform2f(const char *name, float x, float y);
-void vdbUniform3f(const char *name, float x, float y, float z);
-void vdbUniform4f(const char *name, float x, float y, float z, float w);
-void vdbUniform1i(const char *name, int x);
-void vdbUniform2i(const char *name, int x, int y);
-void vdbUniform3i(const char *name, int x, int y, int z);
-void vdbUniform4i(const char *name, int x, int y, int z, int w);
-void vdbUniformMatrix4fv(const char *name, float *x, bool transpose=false);
-void vdbUniformMatrix3fv(const char *name, float *x, bool transpose=false);
+void    vdbLoadShader(int slot, const char *fragment_shader_source_string);
+void    vdbBeginShader(int slot);
+void    vdbEndShader();
+void    vdbUniform1f(const char *name, float x);
+void    vdbUniform2f(const char *name, float x, float y);
+void    vdbUniform3f(const char *name, float x, float y, float z);
+void    vdbUniform4f(const char *name, float x, float y, float z, float w);
+void    vdbUniform1i(const char *name, int x);
+void    vdbUniform2i(const char *name, int x, int y);
+void    vdbUniform3i(const char *name, int x, int y, int z);
+void    vdbUniform4i(const char *name, int x, int y, int z, int w);
+void    vdbUniformMatrix4fv(const char *name, float *x, bool transpose=false);
+void    vdbUniformMatrix3fv(const char *name, float *x, bool transpose=false);
 
 // render texture
-void vdbBeginRenderTexture(int slot, int width, int height, vdbTextureFormat format, int depth_bits=0, int stencil_bits=0);
-void vdbEndRenderTexture(int slot);
-void vdbUnbindRenderTexture();
-void vdbBindRenderTexture(int slot, vdbTextureFilter filter=VDB_LINEAR, vdbTextureWrap wrap=VDB_CLAMP);
-void vdbDrawRenderTexture(int slot, vdbTextureFilter filter=VDB_LINEAR, vdbTextureWrap wrap=VDB_CLAMP);
+void    vdbBeginRenderTexture(int slot, int width, int height, vdbTextureFormat format, int depth_bits=0, int stencil_bits=0);
+void    vdbEndRenderTexture(int slot);
+void    vdbUnbindRenderTexture();
+void    vdbBindRenderTexture(int slot, vdbTextureFilter filter=VDB_LINEAR, vdbTextureWrap wrap=VDB_CLAMP);
+void    vdbDrawRenderTexture(int slot, vdbTextureFilter filter=VDB_LINEAR, vdbTextureWrap wrap=VDB_CLAMP);
 
 // These generate easily accessible GUI elements. Useful for simple prototypes.
-float vdbSliderFloat(const char *name, float vmin, float vmax, float v_init);
-int   vdbSliderInt(const char *name, int vmin, int vmax, int v_init);
-bool  vdbCheckbox(const char *name, bool init);
-bool  vdbRadioButton(const char *name);
-bool  vdbButton(const char *name);
+float   vdbSliderFloat(const char *name, float vmin, float vmax, float v_init);
+int     vdbSliderInt(const char *name, int vmin, int vmax, int v_init);
+bool    vdbCheckbox(const char *name, bool init);
+bool    vdbRadioButton(const char *name);
+bool    vdbButton(const char *name);
 
 // Can't be used in conjunction with built-in scaler.
-void vdbBeginRenderScale(int down, int up);
-void vdbBeginRenderScale(int width, int height, int up);
-void vdbEndRenderScale();
+void    vdbBeginRenderScale(int down, int up);
+void    vdbBeginRenderScale(int width, int height, int up);
+void    vdbEndRenderScale();
 
 // Logging and plotting
-void vdbClearLog(const char *label=0);
-void vdbLogScalar(const char *label, float x, bool overwrite=false); // create new scalar
-void vdbLogArray(const char *label, float x); // append one element to existing array (or create new)
-void vdbLogArray(const char *label, float *x, int columns, bool append=false); // create new array or append to existing (append=true)
-void vdbLogMatrix(const char *label, float *x, int rows, int columns); // create new matrix
-void vdbLogMatrix(const char *label, float **x, int rows, int columns); // create new matrix
-void vdbLogMatrixRow(const char *label, float *x, int columns); // append row to existing matrix (or create new)
-void vdbLogMatrixCol(const char *label, float *x, int rows); // append column to existing matrix (or create new)
-void vdbLogMatrixTranspose(const char *label, float *x, int rows, int columns); // create new matrix
-void vdbLogMatrixTranspose(const char *label, float **x, int rows, int columns); // create new matrix
+void    vdbClearLog(const char *label=0);
+void    vdbLogScalar(const char *label, float x, bool overwrite=false); // create new scalar
+void    vdbLogArray(const char *label, float x); // append one element to existing array (or create new)
+void    vdbLogArray(const char *label, float *x, int columns, bool append=false); // create new array or append to existing (append=true)
+void    vdbLogMatrix(const char *label, float *x, int rows, int columns); // create new matrix
+void    vdbLogMatrix(const char *label, float **x, int rows, int columns); // create new matrix
+void    vdbLogMatrixRow(const char *label, float *x, int columns); // append row to existing matrix (or create new)
+void    vdbLogMatrixCol(const char *label, float *x, int rows); // append column to existing matrix (or create new)
+void    vdbLogMatrixTranspose(const char *label, float *x, int rows, int columns); // create new matrix
+void    vdbLogMatrixTranspose(const char *label, float **x, int rows, int columns); // create new matrix
 
 #define VDBB(label) while (vdbBeginFrame(label)) {
 #define VDBE() vdbEndFrame(); }
