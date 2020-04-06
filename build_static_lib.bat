f6e189ef5fa4684b562261bb183d0fb1a133efef
lightbits <simenh.jobb@gmail.com>
2020-04-06 07:45:18 +0200

    Update build script: omit debug information

--- build_static_lib.bat
+++ build_static_lib.bat
@@ -4,7 +4,7 @@ if not exist "lib" mkdir lib
 pushd lib
 
 set INCLUDES=-I..\include\ -I..\include\vdb\ -I%SDL2_DIR%\include -I..\src\freetype\include
-set FLAGS=-EHsc -MD -Zi -Oi -fp:fast -nologo -O2 -WX -W3 -wd4100 -wd4189 -wd4996 -wd4055
+set FLAGS=-EHsc -MD -Oi -fp:fast -nologo -O2 -WX -W3 -wd4100 -wd4189 -wd4996 -wd4055
 
 cl %INCLUDES% %FLAGS% -c ..\src\vdb.cpp
 lib *.obj -nologo -out:vdb.lib
